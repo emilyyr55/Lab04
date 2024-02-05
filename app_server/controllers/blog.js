fbfafff068913603f6eeba769f261a33480ad1d0
Emily Riley <emilyyr55@gmail.com>
2024-02-05 09:44:44 -0500

    First commit

---
+++ app_server/controllers/blog.js
@@ -0,0 +1,9 @@
+/* GET 'Location info' page */
+module.exports.blogList = function(req, res) {
+    res.render('blog-list', { title: 'Blog List'});
+};
+
+/* GET 'Add review' page */
+module.exports.addBlog = function(req, res){
+    res.render('add-blog', { title : 'Blog Add'});
+};
---
+++ app_server/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var ctrlBlogs = require('../controllers/blog');
+var ctrlOthers = require('../controllers/others');
+
+/* Location pages */
+router.get('/', ctrlOthers.home);
+router.get('/blog', ctrlBlogs.blogList);
+router.get('/blog/add', ctrlBlogs.addBlog);
+
+module.exports = router;
