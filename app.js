c2913e8923b0579aad9e8d72f88e4696cac85b46
Emily Riley <emilyyr55@gmail.com>
2024-02-05 11:27:40 -0500

    updated navbar

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/public/stylesheets'));
+app.use('/webfonts', express.static(__dirname + '/public/fonts/webfonts/'));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
